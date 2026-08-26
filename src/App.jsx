79e0819d7da402c7d8d7d98951ac80529fa7e69f
Luiz Felipe <luiznovafluiz@gmail.com>
2026-08-26 19:25:11 -0300

    Chore: realiza configurações iniciais do projeto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Footer from './Components/Footer';
+import Login from './Components/Login/Login';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}  />
+        <Route path="/login" element={<Login />}  />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      Essa é a parte de login
+    </div>
+  )
+}
+
+export default Login
