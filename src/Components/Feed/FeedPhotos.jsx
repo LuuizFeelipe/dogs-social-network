74219886be6aa8eaffa5a500ad1e7b925f42fe0a
Luiz Felipe <luiznovafluiz@gmail.com>
2026-09-22 14:07:35 -0300

    Feat: adiciona scroll infinito

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -6,18 +6,21 @@ import Error from '../Helper/Error.jsx';
 import Loading from '../Helper/Loading.jsx';
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = ({setModalPhoto}) => {
+const FeedPhotos = ({page, user, setModalPhoto, setInfinite}) => {
 
   const { request, data, error, loading } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({page: 1, total: 6, user: 0});
-      const { json } = await request(url, options);
-      console.log(json);
+      const total = 6;
+      const { url, options } = PHOTOS_GET({page: page, total: total, user: user});
+      const { json, response } = await request(url, options);
+      if(response && response.ok && json.length < total) {
+        setInfinite(false);
+      }
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
 
   if(error) return <Error error={error} />
   if(loading) return <Loading />
--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -4,13 +4,16 @@ import { Route, Routes } from 'react-router-dom'
 import Feed from '../Feed/Feed.jsx'
 import UserPhotoPost from './UserPhotoPost.jsx'
 import UserStats from './UserStats.jsx'
+import { UserContext } from '../../Context/User/UserContext.jsx'
 
 const User = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path='/' element={<Feed />} ></Route>
+        <Route path='/' element={<Feed user={data.id} />} ></Route>
         <Route path='/postar' element={<UserPhotoPost />} ></Route>
         <Route path='/estatisticas' element={<UserStats />} ></Route>
       </Routes>
