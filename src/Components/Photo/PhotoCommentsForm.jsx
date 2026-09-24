56301c7130e8f440ef11f327ca1caf1527fd2a5d
Luiz Felipe <luiznovafluiz@gmail.com>
2026-09-24 15:03:07 -0300

    Feat: adiciona página individual de cada foto

--- src/Components/Photo/PhotoCommentsForm.jsx
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -5,7 +5,7 @@ import { COMMENT_POST } from '../../api';
 import Error from '../Helper/Error';
 import styles from './PhotoCommentsForm.module.css';
 
-const PhotoCommentsForm = ({id, setComments}) => {
+const PhotoCommentsForm = ({id, setComments, single}) => {
   const [comment, setComment] = React.useState("");
   const { request, error } = useFetch();
 
@@ -22,7 +22,7 @@ const PhotoCommentsForm = ({id, setComments}) => {
   }
 1
   return (
-    <form onSubmit={handleSubmit} className={styles.form}>
+    <form onSubmit={handleSubmit} className={`${styles.form} ${single ? styles.single : ''}`}>
       <textarea
         className={styles.textarea}
         name="comment" 
